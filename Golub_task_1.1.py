cdecc4841b981401b12f919deee4e7a8eba46c3d
oleggolub1971 <oleggolub@mail.ru>
2023-03-09 18:58:25 +0300

    task added

---
+++ Golub_task_1.1.py
@@ -0,0 +1,9 @@
+# Задача 1.1.
+
+# Есть строка с перечислением песен
+
+my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
+
+# Выведите на консоль с помощью индексации строки, последовательно: первый трек, последний, второй, второй с конца
+# Нельзя переопределять my_favorite_songs и запятая не должна выводиться.
+print(my_favorite_songs[:14], my_favorite_songs[64:], my_favorite_songs[16:30], my_favorite_songs[51:62])
