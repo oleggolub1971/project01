a1eb2a6fa89a3958636a5cb6ddc66096e8e0f0f2
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2023-04-04 11:48:12 +0300

    Выполнено

--- Golub_task_2.3.py
+++ Golub_task_2.3.py
@@ -29,3 +29,10 @@ try:
 
 except KeyError:
     a = 0
+
+# Отлично) через try-except можно,
+# мое решение как всегда через словари
+
+def switch_it_up(number):
+    return {0: 'Zero', 1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five',
+            6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine'}.get(number)
